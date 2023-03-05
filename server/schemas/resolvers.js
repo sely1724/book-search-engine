ffd0da05c123636574ead1072f60763d439bf7ac
Sylvia Ely <sylvianne9417@gmail.com>
2023-03-05 11:06:39 -0800

    think ? I have typedefs done based on readme specifications

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,68 @@
 const { AuthenticationError } = require("apollo-server-express");
-
-const { User } = require("../models");
+const { GraphQLError } = require("graphql");
+const { User, Book } = require("../models");
+const bookSchema = require("../models/Book");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  Query: {},
+  Query: {
+    users: async () => {
+      return User.find();
+    },
 
-  Mutation: {},
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username }) => {
+      return User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email }); // is email what we want? Copied from activity 26
+
+      if (!user) {
+        throw new GraphQLError("No profile with meail found", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new GraphQLError("Incorrect password", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async(),
+
+    removeBook: async (parent, { bookId }, context) => {
+      // to do add all dependencies
+      if (context.user) {
+        // return Book
+      }
+    },
+  },
 };
+
+// login: Accepts an email and password as parameters; returns an Auth type.
+
+// addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+
+// saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+
+// removeBook: Accepts a book's bookId as a parameter; returns a User type.
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,36 @@
 const typeDefs = `#graphql
-//   type NAME { <-- TODO CHANGE
-  
+  type Query {
+   me: User
   }
 
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
+  type Book {
+    bookId: String 
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookToSave: bookInput): User
+    removeBook(bookId: String!): User
+  }
 `;
 
 module.exports = typeDefs;
